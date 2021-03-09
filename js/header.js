d50ef0dc0bef2c075b96ecbdc65bc7b38af12100
fxxksky <1016939850@qq.com>
2021-03-09 23:07:38 +0800

    commit to 'main'

--- js/header.js
+++ js/header.js
@@ -11,3 +11,18 @@
 //         this.style.backgroundColor = 'red';
 //     })
 // }
+var tab = document.querySelector('.tab_list');
+var lis = tab.querySelectorAll('li');
+var as = tab.querySelectorAll('a');
+for (var i = 0; i < lis.length; i++) {
+    lis[i].addEventListener('click', function() {
+        for (var i = 0; i < lis.length; i++) {
+            lis[i].style.backgroundColor = '';
+        }
+        for (var i = 0; i < as.length; i++) {
+            as[i].style.color = '#666';
+            as[i].style.textDecoration = 'none';
+        }
+        this.style.backgroundColor = 'rgb(54,54,54)';
+    })
+}
