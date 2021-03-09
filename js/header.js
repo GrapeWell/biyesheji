470cc7ab07c5417778501d866566eb240376bed4
GrapeWell <51957597+GrapeWell@users.noreply.github.com>
2021-03-09 19:38:03 +0800

    2021/3/9第一次提交

---
+++ js/header.js
@@ -0,0 +1,13 @@
+// 点击谁就给谁绑定外观
+
+
+// var lis = tab[0].getElementsByTagName('li');
+
+// for (var i = 0; i < lis.length; i++) {
+//     lis[i].addEventListener('click', function() {
+//         for (var i = 0; i < lis.length; i++) {
+//             lis[i].style.backgroundColor = '';
+//         }
+//         this.style.backgroundColor = 'red';
+//     })
+// }
